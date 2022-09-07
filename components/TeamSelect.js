db48e83c2fc5c1f684c538f82956571f7f99b859
robertwhite22 <robertwhite.dev@gmail.com>
2022-09-07 04:59:49 -0400

    home page iphone landscape

--- components/TeamSelect.js
+++ components/TeamSelect.js
@@ -2,11 +2,11 @@ import TeamSelectTeam from './TeamSelectTeam'
 
 function TeamSelect({ teams }) {
   return (
-    <section className='team-select'>
+    <>
       {teams.map((team, index) => (
         <TeamSelectTeam key={team.id} team={team} index={index} />
       ))}
-    </section>
+    </>
   )
 }
 export default TeamSelect
