77da04710f2e6ebca8328096b68ce40e75bd5075
robertwhite22 <robertwhite.dev@gmail.com>
2022-09-06 23:37:36 -0400

    added styles

--- components/TeamSelect.js
+++ components/TeamSelect.js
@@ -3,8 +3,8 @@ import TeamSelectTeam from './TeamSelectTeam'
 function TeamSelect({ teams }) {
   return (
     <section className='team-select'>
-      {teams.map((team) => (
-        <TeamSelectTeam key={team.id} team={team} />
+      {teams.map((team, index) => (
+        <TeamSelectTeam key={team.id} team={team} index={index} />
       ))}
     </section>
   )
--- components/TeamSelectTeam.js
+++ components/TeamSelectTeam.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
-function TeamSelectTeam({ team }) {
+function TeamSelectTeam({ team, index }) {
+  console.log((index + 1) % 2)
   return (
-    <div className='team-select-team'>
+    <div
+      className={
+        (index + 1) % 2 ? 'team-select-team' : 'team-select-team even'
+      }>
       <h2>{team.name}</h2>
       <div className='img-container'>
         <Image src={`/${team.id}.png`} height={100} width={140} />
